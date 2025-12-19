9ae26baba7858bad61fde536171fcf752726233a
Ayaan Mirza Baig <80091173+gitcomit8@users.noreply.github.com>
2025-12-19 18:15:28 +0000

    feat: finish kvm_api with all necessary functions
    - Add fn to get mmap_size of cpu for vm
    - Add fn to create vm fd and return it

--- src/kvm_api.rs
+++ src/kvm_api.rs
@@ -1,14 +1,26 @@
-use kvm_ioctls::{Kvm,Error};
+use kvm_ioctls::{Kvm, Error, VmFd};
 
 struct Hypervisor {
+	//Hold the kvm instance to keep /dev/kvm open
 	kvm: Kvm,
 }
 
 impl Hypervisor {
+
 	pub fn new()-> Result<Self,Error>{
 		// Open /dev/kvm
 		let kvm = Kvm::new()?;
 		//Using ? instead of unwrap() to propagate error on failure
 		Ok(Self{ kvm, })
+	}
+
+	pub fn get_vcpu_mmap_size(&self) ->Result<usize,Error>{
+		// Call helper method on self.kvm
+		self.kvm.get_vcpu_mmap_size()
+	}
+
+	pub fn create_vm(&self) -> Result<VmFd,Error> {
+		// Returns specialized fd that represents te VM
+		self.kvm.create_vm()
 	}
 }
